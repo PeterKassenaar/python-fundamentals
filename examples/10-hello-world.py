7a7e9093a63048acac2c68ad910a9ff284c50213
PeterKassenaar <info@kassenaar.com>
2022-10-14 10:58:05 +0200

    Added example 10-hello-world

---
+++ examples/10-hello-world.py
@@ -0,0 +1,12 @@
+# Some simple examples in a small file.
+# You can use these commands in the REPL, or 
+# in a file and use the command python 10-hello-world.py
+# to execute them.
+print ('Hello World')
+print (2 + 2)
+a = 10;
+print (a)
+if a >= 10:
+    print('a is bigger than, or equal to 10');
+
+# your additional code here...
