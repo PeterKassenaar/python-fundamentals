88612e52f08fdd49e068b4fb93860e319a6776e5
PeterKassenaar <info@kassenaar.com>
2022-10-27 13:45:12 +0200

    Added Flask Examples

---
+++ examples/310-flask-application.py
@@ -0,0 +1,3 @@
+# Instead of having a Full Flask Application in one file, it is a good
+# idea to distribute it over multiple files. So see directory /Flask-app
+# for files and comments on creating fullstack Flask applications.
